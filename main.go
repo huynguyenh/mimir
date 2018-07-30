be4ed36c1ddb6e4f89d815e4079dbd9235ceb5d0
Huy Nguyen <hoanghuy2908@gmail.com>
2018-07-30 23:45:34 +0700

    detect pr (#1)
    * init commit
    
    * add cli

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 // 1. get PR
 // 2, get file changes
 // 3. do stuff
 // 4. comment to that PR
-
 func main() {
-	fmt.Println("hello world")
+	var branch string
+
+	app := cli.NewApp()
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "branch",
+			Destination: &branch,
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println(branch)
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
